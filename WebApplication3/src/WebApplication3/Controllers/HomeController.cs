9d029978975513740e8f16f779ecd099fdd7e828
parkernielson <parkernielson@gmail.com>
2017-04-12 14:20:05 -0700

    Comments and stubbed out methods to tie into views/models

--- WebApplication3/src/WebApplication3/Controllers/HomeController.cs
+++ WebApplication3/src/WebApplication3/Controllers/HomeController.cs
@@ -16,7 +16,8 @@ namespace WebApplication3.Controllers
         SqlConnection conn = new SqlConnection("Data Source=SQL5019.SmarterASP.NET;Initial Catalog=DB_A16A06_climb;User Id=DB_A16A06_climb_admin;Password=climbdev1;");
 
         public IActionResult Index()
-        {
+        {   
+            
             //Here, fill signed in students from the signed in table in the database
             
 
@@ -53,6 +54,9 @@ namespace WebApplication3.Controllers
         }
         public IActionResult AddClimber(string lastNametoSearch, string firstNameToSearch)
         {
+            //This will eventually correspond to the button for homepage-10, but
+            //for now, it is tied to something else for our tests
+
             //replace this line with adding to the the signed-in Database
             int visitAdded;
             SqlCommand cmd = new SqlCommand("createVisit", conn);
@@ -84,7 +88,7 @@ namespace WebApplication3.Controllers
         }
 
         public IActionResult RemoveClimbers()
-        {
+        {//This corresponds to Homepage-7
              
             string temp = this.Request.Form["signOutCheckBox"];
 
@@ -132,7 +136,7 @@ namespace WebApplication3.Controllers
             // this method should take the card swipe and direct to the sign in page for the appropriate user
             //Student toSignIn;
 
-            //return View("SignInDetails", toSignIn); //SignInDetails doesn't yet expect a student to build the page from
+            //return View("SignInDetails", toSignIn);
         }
 
         public void UserSearch()
@@ -171,6 +175,7 @@ namespace WebApplication3.Controllers
         }
 
         public IActionResult AddCertificationToUser() {
+            //This corresponds to item Homepage-13
 
             //I assume, but may be wrong that
             //this should redirect to a page for adding certifications to users, 
@@ -178,7 +183,44 @@ namespace WebApplication3.Controllers
             return null;
         }
 
+
         //Stubbed out methods for Add Users Page
+        public void TemporaryUserStore() {
+            //This corresponds to item BatchAddUsers-2, when there are more users to add in the current batch
+            //this should hold on to the data to be comitted to the database until it's ready, and
+            //this should reset the page for the next user to be added
+        }
+
+        public IActionResult GroupInfoFinished() {
+            //this corresponds to item BatchAddUsers-2, when on the final user for the current batch
+            //this should hold on to the data to be comitted to the database until it's ready, and
+            //this should redirect to (I think) the page with the appropriate training video
+
+            return null;
+
+
+        }
+
+        public IActionResult InstructionalVideoFinished()
+        {   //this corresponds to item BatchAddUsers-5, the call should be restricted on the JS/HTML side of things (I think)
+            //this should retrieve the data for the current group watching, and
+            //this should return the waiver signing page for this group
+            return null;
+        }
+
+        public void RegisterUser() {
+            //this corresponds to item BatchAddUsers-7, when there are more users to add in the current batch
+            //this should commit user data and the waiver to the database, and
+            //this should prepare the page for the next user
+        }
+
+        public void RegisterFinalUser(){
+            //this corresponds to item BatchAddUsers-7, when on the final user
+            //this should commit user data and the waiver to the database and
+            //return to which page? this is currently unknown by Parker.
+
+        }
+
 
     }
 
