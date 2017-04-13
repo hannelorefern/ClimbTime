2ed0dc70d721c258b1ad41d7aa6b4a0a725f0d5f
Kera Parsons <keralyn_ruth@hotmail.com>
2017-04-13 10:28:15 -0700

    DataAccesser object added, but not yet complete.

---
+++ WebApplication3/src/WebApplication3/Models/Certification.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Models
+{
+    public class Certification
+    {
+        public int ID { get; set; }
+        public string title { get; set; }
+        public int yearsBeforeExp { get; set; }
+
+        public Certification()
+        {
+            ID = 0;
+            title = null;
+            yearsBeforeExp = 0;
+        }
+    }
+}
