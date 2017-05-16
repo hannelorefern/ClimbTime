b437bee40c3fc48d3c5dad1928b32011b1798063
parkernielson <parkernielson@gmail.com>
2017-05-15 18:29:31 -0700

    Passin that data from the controller to the page back to the controller like a cool guy

--- WebApplication3/src/WebApplication3/Controllers/HomeController.cs
+++ WebApplication3/src/WebApplication3/Controllers/HomeController.cs
@@ -123,8 +123,39 @@ namespace WebApplication3.Controllers
 
         }
 
-        
-       
+        public IActionResult TestMethod(List<User> userList)
+        {
+            //TO DO: assign a default user type to new users
+
+            string temp = this.Request.Form["firstNameField"];
+            string[] firstNames = temp.Split(',');
+            temp = this.Request.Form["lastNameField"];
+            string[] lastNames = temp.Split(',');
+            temp = this.Request.Form["phoneField"];
+            string[] phones = temp.Split(',');
+            temp = this.Request.Form["addressField"];
+            string[] addresses = temp.Split(',');
+            temp = this.Request.Form["cardswipeField"];
+            string[] cardswipes = temp.Split(',');
+            List<User> users = new List<User>();
+            for (int i = 0; i < firstNames.Length; i++)
+            {
+                User toAdd = new WebApplication3.User();
+                toAdd.firstName = firstNames[i];
+                toAdd.lastName = lastNames[i];
+                toAdd.phoneNumber = phones[i];
+                toAdd.email = addresses[i];
+                toAdd.studentID = cardswipes[i];
+                //toAdd.userType = **default VALUE**
+
+
+                users.Add(toAdd);
+            }
+
+            return View("EmbeddedVideo", users);
+        }
+
+
 
         public IActionResult SignInClimber(string CardSwipe) {
 
